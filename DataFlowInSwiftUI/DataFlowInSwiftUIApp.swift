e544683fdbf9a7bea7ccbe5b91606d9e9d01f42f
Brian McIntosh <brian@mixinteractive.com>
2023-03-03 14:13:17 -0500

    Initial Commit

---
+++ DataFlowInSwiftUI/DataFlowInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowInSwiftUIApp.swift
+//  DataFlowInSwiftUI
+//
+//  Created by Brian McIntosh on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
